ba67649d7910a74f9cf2e7085c8c7a35c675e526
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-02-16 16:22:42 -0500

    env

--- http.js
+++ http.js
@@ -1,13 +1,14 @@
 const http = require('node:http');
 const { findAvailablePort } = require('./free-port.js');
 
+const desiredPort = process.env.PORT ?? 3000;
 
 const server = http.createServer((req, res) => {
     console.log('request received');
     res.end('Hello World\n');
 });
 
-findAvailablePort(3000).then(port => {
+findAvailablePort(desiredPort).then(port => {
     server.listen(port, () => {
         console.log(`Server listening on port http://localhost:${server.address().port}`);
     });
