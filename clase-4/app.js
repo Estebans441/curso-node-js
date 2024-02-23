dabcb599960ad22490f295e1c53573e2bb4700fd
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-02-22 22:59:31 -0500

    dependency injection

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,21 +1,23 @@
 import express, { json } from 'express'; // require --> commonJS
-import { moviesRouter } from './routes/movies.js';
+import { createMovieRouter } from './routes/movies.js';
 import { corsMiddleware } from './middlewares/cors.js';
 
-const app = express();
-app.disable('x-powered-by'); // Deshabilita el header X-Powered-By
+export const createApp = ({ movieModel }) => {
+    const app = express();
+    app.disable('x-powered-by'); // Deshabilita el header X-Powered-By
 
-// npx servor ./web
+    // npx servor ./web
 
-// Middlewares
-app.use(json());
-app.use(corsMiddleware());
+    // Middlewares
+    app.use(json());
+    app.use(corsMiddleware());
 
-// Peliculas
-app.use('/movies', moviesRouter);
+    // Peliculas
+    app.use('/movies', createMovieRouter({ movieModel }));
 
 
-const PORT = process.env.PORT ?? 1234;
-app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`);
-});
+    const PORT = process.env.PORT ?? 1234;
+    app.listen(PORT, () => {
+        console.log(`Server listening on port http://localhost:${PORT}`);
+    });
+}
--- clase-4/controllers/movies.js
+++ clase-4/controllers/movies.js
@@ -1,25 +1,29 @@
-import { MovieModel } from '../models/mysql/movie.js';
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js';
 
 /**
  * Controlador de películas.
  */
 export class MovieController {
+
+    constructor({ movieModel }) {
+        this.movieModel = movieModel;
+    }
+
     /**
      * Obtiene todas las películas filtradas por género.
      */
-    static async getAll (req, res) {
+    getAll = async (req, res) => {
         const { genre } = req.query;
-        const movies = await MovieModel.getAll({ genre });
+        const movies = await this.movieModel.getAll({ genre });
         return res.json(movies);
     }
 
     /**
      * Obtiene una película por su ID.
      */
-    static async getById (req, res) {
+    getById = async (req, res) => {
         const id = req.params.id;
-        const movie = await MovieModel.getById({ id });
+        const movie = await this.movieModel.getById({ id });
     
         if (movie) res.json(movie);
         else return res.status(404).json({ message: 'Movie not found' });
@@ -28,13 +32,13 @@ export class MovieController {
     /**
      * Crea una nueva película.
      */
-    static async create (req, res) {
+    create = async (req, res) => {
         const result = validateMovie(req.body);
         if(result.error) {
             return res.status(400).json({ message: result.error });
          }
          
-         const newMovie = await MovieModel.create({ input : result.data});
+         const newMovie = await this.movieModel.create({ input : result.data});
          if(newMovie) return res.status(201).json(newMovie);
          else return res.status(404).json({ message: 'Movie not found' });
      }
@@ -42,9 +46,9 @@ export class MovieController {
      /**
      * Elimina una película por su ID.
      */
-     static async delete (req, res) {    
+    delete = async (req, res) => {    
         const {id} = req.params;
-        const deleted = await MovieModel.delete({ id });
+        const deleted = await this.movieModel.delete({ id });
     
         if (deleted) return res.json({ message: 'Movie deleted' });
         else return res.status(404).json({ message: 'Movie not found' });
@@ -53,13 +57,13 @@ export class MovieController {
     /**
      * Actualiza una película por su ID.
      */
-    static async update (req, res) {
+    update = async (req, res) => {
         const { id } = req.params;
         const result = validatePartialMovie(req.body);
         
         if(!result.success) return res.status(400).json({ message: result.error });
         
-        const updatedMovie = await MovieModel.update({ id : id,  data : result.data });
+        const updatedMovie = await this.movieModel.update({ id : id,  data : result.data });
         return res.json(updatedMovie);
     }
 }
